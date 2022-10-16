ef60b32e96f348ab03400153d5fac41314632a6a
alexd <alex.dvorovenko@gmail.com>
2022-10-16 21:29:02 +0200

    I make all the program.Which try yo find the meaning of Banana

--- src/com/shop/Store.java
+++ src/com/shop/Store.java
@@ -6,11 +6,31 @@ package com.shop;
 //- В этом класе создать один метод с названием isInStock, который как параметр принимает строку и возвращает boolean.
 //- Этот метод будет проверять содержить ли массив строку, которая передана как аргумент метода isInStock
 
-public class Shop {
+import java.util.Arrays;
+
+public class Store {
+
+
+    static String[] products = {"Apple", "Milk", "Chocolate", "Eggs", "Banana"};
+
+    //static String a = "Banana";
+
+    public boolean isInStock(String str) {
+        //if (str == products){
+        return true;
+        //}
+        //return false;
+    }
 
     public static void main(String[] args) {
-
-
+        System.out.println(Arrays.toString(products));
+        for (int i = 0; i < products.length; i++) {
+            System.out.println(products[i]);
+            if (products[i].equals("Banana")) {
+                System.out.println("We find it");
+            }
+        }
     }
 
 }
+
